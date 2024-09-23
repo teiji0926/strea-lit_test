1a9d4263929dcb6e4a7a4df51f41ab59de5f4ecd
teiji0926 <118594404+teiji0926@users.noreply.github.com>
2024-09-23 17:26:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import streamlit as st
+import pandas as pd
 
 # ①社員番号の入力画面
 st.title('社員情報検索システム')
 st.subheader('社員番号を入力してください')
+
+employee_data = {
+    '社員番号': ['100001', '100002', '100003'],
+    '名前': ['花子さん', '太郎さん', '次郎さん'],
+    'EX': ['〇', '×', '×'],
+    'e-5489': ['×', '〇', '〇'],
+    'えきねっと': ['×', '×', '〇']
+}
+df = pd.DataFrame(employee_data)
 
 # ユーザーに社員番号を入力させる
 employee_id = st.text_input('社員番号')
@@ -10,5 +20,6 @@ employee_id = st.text_input('社員番号')
 # ②エンターボタン
 if st.button('検索') :
     st.write('成功')
+    st.table(df)
 else:
     st.write('社員番号を入れて検索ボタンを押下くだせえ')
